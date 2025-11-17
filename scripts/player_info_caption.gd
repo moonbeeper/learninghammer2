d68056206e667ec0c336d8d82e5395a9d8281492
moonbeeper <46693178+moonbeeper@users.noreply.github.com>
2025-11-17 03:14:40 +0100

    hint now accepts using %% to show a keybind

--- scripts/player_info_caption.gd
+++ scripts/player_info_caption.gd
@@ -1,6 +1,10 @@
 extends PanelContainer
 
 @export var label: RichTextLabel
+@export var with_key_name_container: HBoxContainer
+@export var key_name_label: Label
+@export var key_label1: RichTextLabel
+@export var key_label2: RichTextLabel
 
 var is_active: bool = false
 var timer: Timer
@@ -28,7 +32,13 @@ func _on_show_instructor_hint(hint: String, time: float) -> void:
 	if is_active: await tween_to_hide()
 	
 	is_active = true
-	label.text = hint
+	
+	if hint.contains("%"):
+		set_split_key_string(hint)
+	else:
+		label.visible = true
+		with_key_name_container.visible = false
+		label.text = hint
 	#reset_pivot()
 	
 	start_timer()
@@ -62,4 +72,34 @@ func tween_to_show() -> void:
 #func reset_pivot() -> void: 
 	#var current_size = size
 	#pivot_offset = Vector2(current_size.x/2, current_size.y/2)
+
+func get_key_name(action: String) -> String:
+	print(action)
+	var maps = InputMap.action_get_events(action)
+	print(maps)
+	if maps.is_empty():
+		return "???"
+		
+	var bind = maps[0]
+	if bind is InputEventKey:
+		return OS.get_keycode_string(bind.physical_keycode)
+	else:
+		return "???"
+		
+func set_split_key_string(string: String) -> void:
+	var start_idx = string.find("%")
+	var end_idx = string.find("%", start_idx+1)
+	
+	var action_name = string.substr(start_idx + 1, end_idx-start_idx-1) 
+	var key_name = get_key_name(action_name)
+	
+	var before_keyname: String = string.substr(0, start_idx - 1).strip_edges()
+	var after_keyname: String = string.substr(end_idx + 1).strip_edges()
+	
+	label.visible = false
+	with_key_name_container.visible = true
+	
+	key_label1.text = before_keyname
+	key_label2.text = after_keyname
+	key_name_label.text = key_name
 	
